4ec7c66235eb0870815031203f3d768e497ebdf7
Mani <manibabusrkr@gmail.com>
2023-09-15 19:51:56 +0530

    terraform

---
+++ vars/vars.tf
@@ -0,0 +1,40 @@
+variable "course" {
+  default = "DevOps Training"
+}
+
+# List
+variable "courses" {
+  default = [
+    "DevOps",
+    "AWS",
+    "Python"
+  ]
+}
+
+## Map
+variable "course_details" {
+  default = {
+    DevOps = {
+      name     = "DevOps"
+      timing   = "10am"
+      duration = 90
+    }
+    AWS = {
+      name     = "AWS"
+      timing   = "11am"
+      duration = 30
+    }
+  }
+}
+
+output "course" {
+  value = var.course
+}
+
+output "courses" {
+  value = var.courses
+}
+
+output "course_details" {
+  value = var.course_details
+}
