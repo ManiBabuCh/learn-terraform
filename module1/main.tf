4d88ebbb5b78edf17e8d120fbaa9ab1ae6249a67
Mani <manibabusrkr@gmail.com>
2023-09-17 13:00:32 +0530

    terraform

--- module1/main.tf
+++ module1/main.tf
@@ -7,8 +7,8 @@ source ="./ec2"
 name = "instance2"
 }
 output "instance1_public_ip" {
-  value = aws_instance.web.public_ip
+  value = instance1.public_ip
 }
 output "instance2_public_ip" {
-  value = aws_instance.web.public_ip
+  value = instance2.public_ip
 }
