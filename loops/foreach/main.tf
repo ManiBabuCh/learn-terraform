c238a5de8d224ee565addd471500eb0477923dcf
Mani <manibabusrkr@gmail.com>
2023-09-27 23:29:49 +0530

    terraform

---
+++ loops/foreach/main.tf
@@ -0,0 +1,31 @@
+
+resource "aws_instance" "web" {
+  for_each      = var.instances
+  ami           = data.aws_ami.example.id
+  instance_type = lookup(each.value,"instance_type", "t3.micro" )
+
+  tags = {
+    Name = each.key
+  }
+}
+
+
+data "aws_ami" "example" {
+  owners      = ["973714476881"]
+  most_recent = true
+  name_regex  = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+  default = {
+
+    catalogue = {
+      name = catalogueeee
+      instance_type = "t3.nano"
+    }
+
+    frontend={
+      name = frontendddd
+    }
+  }
+}
